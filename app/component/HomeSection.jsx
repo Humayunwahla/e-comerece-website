3af7fe3edd4cd841f6e8ae9071bdeced459a9f8a
Humayun Wahla <humayunwahla55@gmail.com>
2024-12-05 11:24:54 +0500

    set home screen padding and margin

--- app/component/HomeSection.jsx
+++ app/component/HomeSection.jsx
@@ -6,9 +6,9 @@ function HomeSection() {
   return (
     <div>
      <div>
-        <h1 className='text-5xl font-bold mt-20 '>This is the Home Page</h1>
+        <h1 className='text-5xl font-bold mt-7 '>This is the Home Page</h1>
      </div>
-     <div className='grid grid-cols-2 justify-between pt-20 px-4'>
+     <div className='grid grid-cols-2 justify-between pt-10 px-4'>
         <div className=' mx-auto text-center mt-32'> 
           <h1 className='text-3xl mb-4 font-bold'>This is it&apos;s main Heading</h1>
           <p className='text-left'>Welcome to our platform! We strive to provide a seamless experience, offering tools and resources to meet your needs. Explore our features, stay updated, and connect with a community that shares your interests. Start your journey with us today!</p>
